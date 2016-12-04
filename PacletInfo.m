b9c331f94dca6bf9d367967139769a3974680507
Zach Bjornson <bjornson@stanford.edu>
2016-12-04 14:52:49 -0800

    Restructure into workbench proj; Java serialization
    Fixes #12

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,20 +1,17 @@
 (* Paclet Info File *)
 
-(* created 2015/12/29 *)
+(* created 2016/12/08*)
 
 Paclet[
-  Name -> "MongoDBLink",
-  Version -> "0.2.0",
-  MathematicaVersion -> "6+",
-  Extensions ->
-    {
-      {
-        "Documentation",
-        Resources -> {
-          "Guides/MongoDBLink",
-          "ReferencePages/Symbols/DatabaseConnection"
-        },
-        Language -> "English"
-      }
-    }
+    Name -> "MongoDBLink",
+    Version -> "0.2.0",
+    MathematicaVersion -> "6+",
+    Extensions -> 
+        {
+            {"Documentation", Resources -> 
+                {"Guides/MongoDBLink"}
+            , Language -> "English"}
+        }
 ]
+
+
